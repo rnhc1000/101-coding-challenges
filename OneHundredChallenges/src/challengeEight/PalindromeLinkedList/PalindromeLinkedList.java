dab4ab9f323fd604301d5f7dfde66152110cd126
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-14 19:04:55 -0300

    DayTwo of 101 coding challenges - Palindrome, Anagram

---
+++ OneHundredChallenges/src/challengeEight/PalindromeLinkedList/PalindromeLinkedList.java
@@ -0,0 +1,12 @@
+package challengeEight.PalindromeLinkedList;
+
+import java.util.LinkedList;
+
+public class PalindromeLinkedList {
+
+    public static boolean isPalindromic(LinkedList<Integer> list) {
+
+	return true;
+    }
+
+}

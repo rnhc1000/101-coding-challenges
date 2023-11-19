763bf1edb3bbc9ec715268e9e4bfc9351785bd12
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-18 23:22:48 -0300

    Thread Pool + Executor

--- Hello/src/Hi.java
+++ Hello/src/Hi.java
@@ -1,15 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class Hi {
 
     public static void main(String[] args) {
-	int min = 1, max = 10;
+	int min = 1, max = 100;
 	int size = 10;
-	List<Integer> list = new ArrayList<>();
+
+	Set<Integer> list = new HashSet<>();
+	Random random = new Random();
+	size = random.nextInt(100);
+
 	while (size > 0) {
-	    Random random = new Random();
 	    int r = random.nextInt(min, max);
 	    list.add(r);
 	    size--;
@@ -17,8 +20,9 @@ public class Hi {
 	int sumEven = 0;
 	int sumOdd = 0;
 	System.out.println(list);
-	sumEven = list.stream().filter(x -> x % 2 == 0).reduce(0, (x, y) -> x + y);
 	sumOdd = list.stream().filter(x -> x % 2 == 1).reduce(0, (x, y) -> x + y);
+	sumEven = list.stream().filter(x -> x % 2 == 0).reduce(0, (x, y) -> x + y);
+	System.out.println();
 	System.out.println(sumEven);
 	System.out.println(sumOdd);
     }
---
+++ Hello/src/performance/throughput/ThroughputHttpServer.java
@@ -0,0 +1,90 @@
+package performance.throughput;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class ThroughputHttpServer {
+
+    private static final String INPUT_FILE = "/home/rferreira/Downloads/WarAndPeace.txt";
+    private static final int NUMBER_OF_THREADS = 1;
+
+    public static void main(String[] args) {
+	String text = "";
+	try {
+	    text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	startServer(text);
+    }
+
+    public static void startServer(String text) {
+	HttpServer server;
+	try {
+	    server = HttpServer.create(new InetSocketAddress(8080), 0);
+	    server.createContext("/search", new WordCountHandler(text));
+	    Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+	    server.setExecutor(executor);
+	    server.start();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+    }
+
+    private static class WordCountHandler implements HttpHandler {
+	private String text;
+
+	public WordCountHandler(String text) {
+	    this.text = text;
+	}
+
+	public void handle(HttpExchange httpExchange) throws IOException {
+
+	    String query = httpExchange.getRequestURI().getQuery();
+	    String[] keyValue = query.split("=");
+	    String action = keyValue[0];
+	    String word = keyValue[1];
+	    if (action.equals("word")) {
+		httpExchange.sendResponseHeaders(400, 0);
+		return;
+	    }
+
+	    long count = countWord(word);
+	    System.out.println(word);
+	    byte[] response = Long.toString(count).getBytes();
+	    httpExchange.sendResponseHeaders(200, response.length);
+	    OutputStream outputStream = httpExchange.getResponseBody();
+	    outputStream.write(response);
+	    outputStream.close();
+
+	}
+
+	private long countWord(String word) {
+	    long count = 0;
+	    int index = 0;
+	    while (index >= 0) {
+
+		index = text.indexOf(word, index);
+		if (index >= 0) {
+
+		    count++;
+		    index++;
+		}
+
+	    }
+	    return count;
+	}
+
+    }
+}
